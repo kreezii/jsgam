47dd44a3bd4c2c58d793a8401b32e5e25c5c934c
Kreezii <kreeziidaemon@gmail.com>
2019-11-23 22:38:23 +0100

    Initial Web Design

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Adventure from './game';
-
-console.log("JSGAM 5.0.1 https://github.com/kreezii/jsgam");
-/*function Adventure(config){
-  new Game(config);
-}*/
-export default Adventure;
-//export {Game};
